3eb1631e76e2dad960742caea5d7693c4e7f4865
Jose <jose@alsur.es>
2016-09-25 01:13:12 +0200

    add ValidationError when year_of_birth exceeds limits

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 import datetime
 from django.db import models
+from django.core.exceptions import ValidationError
+
 try:
     from django_countries.fields import CountryField
 except ImportError:
@@ -18,6 +20,11 @@ class Driver(models.Model):
     first_name = models.CharField(max_length=25)
     year_of_birth = models.IntegerField()
     country = CountryField()
+
+    def save(self, *args, **kwargs):
+        if self.year_of_birth < 1900 or self.year_of_birth > 2099:
+            raise ValidationError('Year_of_birth must be between 1900 and 2099')
+        super(Driver, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return ', '.join((self.last_name, self.first_name))
