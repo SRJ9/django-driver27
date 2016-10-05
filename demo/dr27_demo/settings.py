387591245e7f16150c53a224f8e8b3875fc38c4b
Jose <jose@alsur.es>
2016-10-05 16:16:55 +0200

    adding settings + sqlite db to driver27 demo

--- demo/dr27_demo/settings.py
+++ demo/dr27_demo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_countries',
+    'driver27',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'dr27_demo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'demo.sqlite3'),
     }
 }
 
--- demo/manage.py
+++ demo/manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+try:
+    import driver27
+except ImportError:
+    sys.path.append(os.path.abspath(os.path.join(os.path.abspath(__file__), '../../')))
+    import driver27
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dr27_demo.settings")
     try:
