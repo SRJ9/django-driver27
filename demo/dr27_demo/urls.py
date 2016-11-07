750a001dd4f621655f1dba343051dd84352a2ab4
Jose E.R <srj9es@gmail.com>
2016-11-08 00:17:41 +0100

    Add index redirect to dr27.

--- demo/dr27_demo/urls.py
+++ demo/dr27_demo/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import index
 
 urlpatterns = [
+    url(r'^$', index),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('driver27.urls'))
 ]
