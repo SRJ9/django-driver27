9aa37051b8bb14d1d68b8633d8cac51f62ad4be1
Jose E.R <srj9es@gmail.com>
2016-11-09 01:19:45 +0100

    Fix tox error changing champion_tag to champion_filter.

--- driver27/templatetags/champion_filter.py
+++ driver27/templatetags/champion_filter.py
@@ -2,7 +2,7 @@ from django import template
 from ..models import Season
 register = template.Library()
 
-@register.simple_tag
-def champion_tag(season_id):
+@register.filter
+def champion_filter(season_id):
     season = Season.objects.get(pk=season_id)
     return '&#9818;' if season.has_champion() else ''
