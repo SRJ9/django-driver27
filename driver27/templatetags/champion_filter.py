71958c94c8c8e04ab63c03480f09d0e528e7769c
Jose E.R <srj9es@gmail.com>
2016-11-11 18:48:24 +0100

    Modify champion_tag css.

--- driver27/templatetags/champion_filter.py
+++ driver27/templatetags/champion_filter.py
@@ -5,4 +5,4 @@ register = template.Library()
 @register.filter
 def champion_filter(season_id):
     season = Season.objects.get(pk=season_id)
-    return '&#9818;' if season.has_champion() else ''
+    return '<span class="champion_tag">&#9818;</span>' if season.has_champion() else ''
