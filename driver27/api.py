c12989eae3f1821d39ce35033f609dd64de09f36
Jose E.R <srj9es@gmail.com>
2016-12-31 01:38:33 +0100

    Add RaceSerializer to API.

--- driver27/api.py
+++ driver27/api.py
@@ -1,18 +1,40 @@
-from .models import Competition, Season
+from .models import Competition, Race, Season
 from rest_framework import routers, serializers, viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
 from django_countries.serializer_fields import CountryField
 
 
+class RaceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Race
+        fields = ('season', 'round', 'round', 'date', 'alter_punctuation')
+
+
+# ViewSets define the view behavior.
+class RaceViewSet(viewsets.ModelViewSet):
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
+
+
 class SeasonSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Season
-        fields = ('year', 'competition', 'round', 'punctuation')
+        fields = ('year', 'competition', 'rounds', 'punctuation', 'races')
 
 
 # ViewSets define the view behavior.
 class SeasonViewSet(viewsets.ModelViewSet):
     queryset = Season.objects.all()
     serializer_class = SeasonSerializer
+
+    @detail_route(methods=['get'])
+    def races(self, request, pk=None):
+        season = self.get_object()
+        self.queryset = season.races.all()
+        serializer = RaceSerializer(instance=self.queryset, many=True, context={'request': request})
+        return Response(serializer.data)
 
 
 class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
@@ -32,4 +54,5 @@ class CompetitionViewSet(viewsets.ModelViewSet):
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'competitions', CompetitionViewSet)
+router.register(r'races', RaceViewSet)
 router.register(r'seasons', SeasonViewSet)
