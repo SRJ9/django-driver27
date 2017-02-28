dcd00019555d8b5731691f0d4a15a2f4535cd9d2
Jose E.R <srj9es@gmail.com>
2017-02-28 12:03:55 +0100

    Update fixtures with f1 seats 2017.

--- driver27/admin/formsets.py
+++ driver27/admin/formsets.py
@@ -5,41 +5,42 @@ from ..models import TeamSeason
 
 
 class RelatedWithSeasonFormSet(forms.models.BaseInlineFormSet):
-    model = None
-    model_attributes = []
-
-    def get_copy(self, copy_id):
-        related_objects = self.model.objects.filter(season__pk=copy_id)
-        initial = []
-        for x in related_objects:
-            initial_x = {}
-            for xa in self.model_attributes:
-                initial_x[xa] = getattr(x, xa, None)
-            initial.append(initial_x)
-        return initial
-
-    def __init__(self, *args, **kwargs):
-        super(RelatedWithSeasonFormSet, self).__init__(*args, **kwargs)
-        request = self.request
-        copy_id = request.GET.get('copy')
-        if not self.initial and copy_id:
-            self.initial = self.get_copy(copy_id)
-            self.extra += len(self.initial)
+    pass
+    # model = None
+    # model_attributes = []
+    #
+    # def get_copy(self, copy_id):
+    #     related_objects = self.model.objects.filter(season__pk=copy_id)
+    #     initial = []
+    #     for x in related_objects:
+    #         initial_x = {}
+    #         for xa in self.model_attributes:
+    #             initial_x[xa] = getattr(x, xa, None)
+    #         initial.append(initial_x)
+    #     return initial
+    #
+    # def __init__(self, *args, **kwargs):
+    #     super(RelatedWithSeasonFormSet, self).__init__(*args, **kwargs)
+    #     request = self.request
+    #     copy_id = request.GET.get('copy')
+    #     if not self.initial and copy_id:
+    #         self.initial = self.get_copy(copy_id)
+    #         self.extra += len(self.initial)
 
 
 class RaceFormSet(RelatedWithSeasonFormSet):
     model = Race
-    model_attributes = ('round', 'circuit', 'grand_prix',)
+    # model_attributes = ('round', 'circuit', 'grand_prix',)
 
 
 class SeatSeasonFormSet(RelatedWithSeasonFormSet):
     model = SeatSeason
-    model_attributes = ('seat',)
+    # model_attributes = ('seat',)
 
 
 class TeamSeasonFormSet(RelatedWithSeasonFormSet):
     model = TeamSeason
-    model_attributes = ('team', 'season', 'sponsor_name',)
+    # model_attributes = ('team', 'season', 'sponsor_name',)
 
     def team_season_check(self, form):
         delete_checked = False
