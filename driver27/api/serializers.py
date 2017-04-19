0307e78abbe9b203d8fb6f0522f2665d4eadabca
Jose E.R <srj9es@gmail.com>
2017-04-19 23:18:25 +0200

    Add Giovinazzi to DB

--- driver27/api/serializers.py
+++ driver27/api/serializers.py
@@ -92,14 +92,15 @@ class NestedTeamSerializer(TeamSerializer):
 
 class ContenderSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
-    driver = NestedDriverSerializer(many=False)
-    teams = NestedTeamSerializer(many=True)
+
+    driver_details = NestedDriverSerializer(many=False, read_only=True)
+    teams = NestedTeamSerializer(many=True, read_only=True)
 
     # competition = CompetitionSerializer(many=False)
 
     class Meta:
         model = Contender
-        fields = ('url', 'id', 'driver', 'competition', 'teams')
+        fields = ('url', 'id', 'driver', 'driver_details', 'competition', 'teams')
 
 
 #
