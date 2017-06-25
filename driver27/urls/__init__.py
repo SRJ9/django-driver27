ae5fe7afaf61c2d3631fdb5adc3df6e4aa971d06
Jose E.R <srj9es@gmail.com>
2017-06-25 16:25:50 +0200

    Update fixtures until Canada 2017 and fix URLs order.

--- driver27/urls/__init__.py
+++ driver27/urls/__init__.py
@@ -6,7 +6,7 @@ from ..api import router
 urlpatterns = [
     url(r'^$', views.global_view, name='dr27-global-view'),
     url(r'^global/', include('driver27.urls.global')),
-    url(r'^(?P<competition_slug>[-\w\d]+)/', include('driver27.urls.competition')),
     url(r'^api/auth/', include('rest_framework.urls')),
     url(r'^api/', include(router.urls)),
+    url(r'^(?P<competition_slug>[-\w\d]+)/', include('driver27.urls.competition')),
 ]
