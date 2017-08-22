427c69a336be7f1ad8064d27672f239061a83c2e
Jose E.R <srj9es@gmail.com>
2017-08-22 20:14:54 +0200

    Add help_text in SeasonAdminForm.

--- driver27/admin/forms.py
+++ driver27/admin/forms.py
@@ -5,12 +5,13 @@ from ..models import Season
 
 
 class SeasonAdminForm(AlwaysChangedModelForm):
-
     def __init__(self, *args, **kwargs):
         super(SeasonAdminForm, self).__init__(*args, **kwargs)
         punctuation_choices = get_punctuation_label_dict()
         self.fields['punctuation'] = forms.ChoiceField(choices=BLANK_CHOICE_DASH + list(punctuation_choices),
                                                        initial=None)
+        self.fields['rounds'].help_text = 'If \'rounds\' are not empty, only the best (rounds) results will be ' \
+                                          'counted for driver points'
 
     class Meta:
         model = Season
