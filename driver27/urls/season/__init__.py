0ea7b47c2ed2b12ab7cee9a29e49849566e264fb
Jose E.R <srj9es@gmail.com>
2017-09-18 16:53:29 +0200

    Fix url.

--- driver27/urls/season/__init__.py
+++ driver27/urls/season/__init__.py
@@ -4,7 +4,7 @@ from driver27 import views
 
 urlpatterns = [
     url(r'^$', views.season_view, name='view'),
-    url(r'^driver/position-draw/$', views.driver_season_pos_view, name='dr27-season-position-draw'),
+    url(r'^driver/position-draw/$', views.driver_season_pos_view, name='position-draw'),
     url(r'^driver/', include('driver27.urls.driver')),
     url(r'^team/', include('driver27.urls.team')),
     url(r'^race/', include('driver27.urls.season.race')),
