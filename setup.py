f044bdb6a5fabf6a02cfb3d4af65b0a9cc653e62
Jose E.R <srj9es@gmail.com>
2017-10-21 22:29:02 +0200

    DRF forces to delete compatibility with versions < 1.10

--- setup.py
+++ setup.py
@@ -28,10 +28,8 @@ setup(
         #   5 - Production/Stablegit
         'Development Status :: 4 - Beta',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
-        # 'Framework :: Django :: 1.11',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
@@ -39,10 +37,9 @@ setup(
         # # Specify the Python versions you support here. In particular, ensure
         # # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        # 'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6'
     ]
 
 )
