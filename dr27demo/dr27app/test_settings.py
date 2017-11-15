8c37c1ae7f42d22a186de4751a56209ebdd77ec4
Jose E.R <srj9es@gmail.com>
2017-11-15 23:19:07 +0100

    Add settings for django tests.

---
+++ dr27demo/dr27app/test_settings.py
@@ -0,0 +1,10 @@
+from .base_settings import *
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'KEY_PREFIX': 'driver27-cache-app'
+
+    }
+}
